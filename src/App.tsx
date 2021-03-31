733d447226532e84adfb4d4c4ae2d2f997b91dc2
Lilimly <aureliemlynarz@gmail.com>
2021-03-31 12:02:17 +0200

    implement the app fonctions

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,111 @@
 import React, { useState } from 'react';
 //components
-import QuestionCard from './components/QuestionCard'
-import { fetchQuizQuestions } from './API'
+import QuestionCard from './components/QuestionCard';
+import { fetchQuizQuestions } from './API';
 
 //Types
-import { Difficulty } from './API'
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setloading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
-
   const startTrivia = async () => {
+    setloading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setloading(false);
 
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
+    if(!gameOver) {
+      // get users answer
+      const answer = e.currentTarget.value;
+      // check if the answer is correct
+      const correct = questions[number].correct_answer === answer;
+      // add score if answer is correct
+      if(correct) setScore(prev => prev + 1);
+      //save answer in the array for user answers (prev = previous state)
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers(prev => [...prev, answerObject]);
+    }
 
   }
 
   const nextQuestion = () => {
+    //move to the next question if it's not the las question
+    const nextQuestion = number + 1;
 
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
 
   }
 
   return (
     <div className="App">
       <h1>REACT QUIZZ</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading questions ...</p>
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next question
-      </button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>
+            Start
+          </button>
+        ) : null
+      }
+      {!gameOver ?
+        <p className="score">Score: {score}</p>
+        : null
+      }
+      {loading && 
+        <p>Loading questions ...</p>
+      }
+      {!loading && !gameOver &&
+        (<QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />)
+      }
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={nextQuestion}>
+            Next question
+          </button>
+        ) : null
+      }
+      
     </div>
   );
 }
